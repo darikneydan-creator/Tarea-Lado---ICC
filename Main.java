877f3d3df1eeb252da3fb82158694e091191349a
darikneydan-creator <darikneydan@ciencias.unam.mx>
2025-09-19 23:38:22 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
 public class Main {
     public static void main(String[] pps) {
-	Punto p1,p3,p4;
-	p1 = new Punto(-5,4.5);
-	Punto p2 = new Punto(-4,3.5);
-	p3= new Punto(-4,4.5);
-	p4 = new Punto (-5,3.5);
+	
+	Punto p1 = new Punto(1, 1);
+        Punto p2 = new Punto(4, 5);
+
+        // Puntos para el Lado 2
+        Punto p3 = new Punto(1, 2);
+        Punto p4 = new Punto(5, 6);
 	//System.out.println(p1.toString());
 	//System.out.println(p1.distancia(new Punto(2,0)));
 	//System.out.println(p1.equals(new Punto(2,5)));
 	Lado lado = new Lado(p1,p2,"Omana");
 	Lado lado2 = new Lado(p3,p4, "Darik");
 	
+	Recta recta1 = new Recta(lado);
+	Recta recta2 = new  Recta(lado2);
 	System.out.println(lado.equals(lado2));
-	System.out.println("Las rectas se intersecan: "+lado.seInterseca(lado2));
-	System.out.println("El punto de interseccion de las rectas es"+lado.interseccionPunto(lado2));
+	System.out.println("Las rectas se intersectan: "+lado.seInterseca(lado2));
+	System.out.println("El punto de la interseccion es: " + recta1.interseccion(recta2));
+
+	
+
 
     }
 }
